c8a173d1280686f7964f9f9d6a90c8a4e362fcd1
apixx <49874926+apixx@users.noreply.github.com>
2021-03-05 10:53:03 +0100

    Add project files.

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        //Declaring a dependency on DataContext service (provides access to the application's data)
+        private DataContext context;
+
+        public ProductsController(DataContext ctx)
+        {
+            context = ctx;
+        }
+
+        // GET
+        [HttpGet]
+        public IEnumerable<Product> GetProducts()
+        {
+            //Requesting all the Product objects from the database
+            return context.Products;
+        }
+
+        //Dependencies declared by action methods must be decorated with [FromService]
+        [HttpGet("{id}")]
+        public Product GetProduct(long id, [FromServices] ILogger<ProductsController> logger)
+        {
+            //Declaring a dependency on ILoger<T>
+            logger.LogDebug("GetProduct Action Invoked");
+            return context.Products.Find(id);
+        }
+
+        [HttpPost]
+        public void SaveProduct([FromBody] Product product)
+        {
+            context.Products.Add(product);
+            context.SaveChanges();
+        }
+
+        [HttpPut]
+        public void UpdateProduct([FromBody] Product product)
+        {
+            context.Products.Update(product);
+            context.SaveChanges();
+        }
+
+        [HttpDelete("{id}")]
+        public void DeleteProduct(long id)
+        {
+            context.Products.Remove(new Product(){ProductId = id});
+            context.SaveChanges();
+        }
+    }
+}
